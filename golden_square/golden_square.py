ecfec11a8dd96eb30d01506f99f9c437d37c666e
Emil Martens <emil.martens@gmail.com>
2020-06-02 22:23:56 +0200

    get square set

--- golden_square/golden_square.py
+++ golden_square/golden_square.py
@@ -9,24 +9,14 @@ Created on Tue Jun  2 13:57:07 2020
 import numba as nb
 from numba import cuda
 import numpy as np
+import time
 
-@nb.cuda.jit('void(u4[:])', cache=True)
-def generation(data):
-    local_data = cuda.shared.array(shape=(10), dtype=nb.uint8)
-    x = cuda.grid(1)
-    tx = cuda.threadIdx.x
-    ty = cuda.threadIdx.y
-
-def solve(a,b,c):
-    return int(-b + np.sqrt(b**2 - 4*a*c) )// (2*a)
-def find_k(diff, target):
-    return int(-2 * target + 2 * np.sqrt(target ** 2 + diff) )//2
-
+N, a, b, c, a_min, b_min, a_rest, b_rest = range(8)
 
 solutions = []
 def get_combinations(number):
     """ Uded to get number of combinations.
-    s_data =  a, b, c, N, d, found_n.
+    s_data =  N, a, b, c, a_min, b_min, a_rest, b_rest
     Parameters
     ----------
     number : int
@@ -36,51 +26,57 @@ def get_combinations(number):
     None.
 
     """
-    s_data = [0 for i in range(6)]
-    a, b, c, N, d, found_n = range(6)
+    s_data = [0 for i in range(8)]
+    N, a, b, c, a_min, b_min, a_rest, b_rest = range(8)
+
+    def sqrt(n):
+        x = n
+        y = 1
+        while(x > y):
+            x = (x+y)//2
+            y = n//x
+        return x
+    def get_a_min():
+        return int(3 + sqrt(3 * s_data[N] - 4)) // 3
+
+    def get_b_min():
+        if s_data[a_rest] == 0:
+            return 1
+        return int(1 + sqrt(2 * s_data[a_rest] - 1)) // 2
+
+
     s_data[N] = number
-    s_data[a], s_data[b], s_data[c] = 1, 2, int(np.sqrt(s_data[N] - 1 - 4))
-    s_data[d] = s_data[N] - (s_data[a]**2 + s_data[b]**2 + s_data[c]**2)
+    s_data[a] = sqrt(s_data[N] - 1)
 
-    while 1:
-        print(s_data)
-        if s_data[d] >= 2 * s_data[a] + 1: # if not possible a, not possible b
-            if s_data[d] >= 2 * s_data[b] + 1:
-                target_increase = b
-            else:
-                target_increase = a
+    s_data[a_rest] = s_data[N] - s_data[a] ** 2
+    s_data[a_min] = get_a_min()
 
-            k = find_k(s_data[d], s_data[target_increase])
-            s_data[d] -= 2 * k * s_data[target_increase] + k**2
-            s_data[target_increase] += k
+    s_data[b] = sqrt(s_data[a_rest])
+    s_data[b_rest] = s_data[a_rest] - s_data[b] ** 2
+    s_data[b_min] = get_b_min()
+
+    s_data[c] = sqrt(s_data[b_rest])
+    while s_data[a] >= s_data[a_min]:
+        if s_data[b_rest] == s_data[c] * s_data[c]:
+            # print(s_data[a]**2 + s_data[b]**2 + s_data[c]**2, s_data[N])
+            print(s_data[1:4])
+        if s_data[b] > s_data[b_min]:
+            s_data[b_rest] += 2 * s_data[b] - 1
+            s_data[b] -= 1
+            s_data[c] = min(s_data[b] - 1, sqrt(s_data[b_rest]))
 
         else:
-            if not s_data[d]:
-                s_data[found_n] += 1
-                solutions.append([s_data[a], s_data[b], s_data[c]])
+            s_data[a_rest] += 2 * s_data[a] - 1
+            s_data[a] -= 1
+            s_data[b] = min(s_data[a] - 1, sqrt(s_data[a_rest]))
 
-            if s_data[c] - s_data[a] <= 2:
-                break
+            s_data[b_rest] = s_data[a_rest] - s_data[b] ** 2
+            s_data[b_min] = get_b_min()
 
-            else:
-                if s_data[c] - s_data[b] > 1:
-                    target_decrease = c
-                else:
-                    target_decrease = b
-
-                s_data[d] += 2 * s_data[target_decrease] - 1
-                s_data[target_decrease] -= 1
-
-
-
-
-        # else:
-        #     if s_data[c] >= s_data[b] + 2:
-        #         s_data[c] -= 1
+            s_data[c] = min(s_data[b] - 1, sqrt(s_data[b_rest]))
 
 
     return s_data
 
 
-print(get_combinations(1424))
-print(solutions)
+get_combinations(6543456)
